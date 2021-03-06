1af2b0ac6d158b07609f1df75bcdc5c4dad07fdf
erenyusufduran <58879282+erenyusufduran@users.noreply.github.com>
2021-03-07 00:07:54 +0300

    Problem Seti 2 - Trafik Işıkları

---
+++ Problem Seti 2 - Trafik Işıkları/problemseti_2_trafikisiklari.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(3,HIGH);
+  delay(10000);
+  digitalWrite(4,HIGH);
+  delay(10000);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  delay(5000);
+  digitalWrite(5,LOW);
+  
+}
